c5d1952c49339934855ad547361b289623647ad1
Hugo-Leyx-valade <hugo.leyx-valade@efrei.net>
2025-04-21 18:26:14 +0200

    dzqdq

--- backend/GreenEscape-backend/api/static/scripts/scripts.js
+++ backend/GreenEscape-backend/api/static/scripts/scripts.js
@@ -164,7 +164,7 @@ document.addEventListener("keydown", (e) => {
       }
       console.log("medails",medails);
     });
-    fetch("api/saveMedals", {
+    fetch("/saveMedals", {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
@@ -198,7 +198,7 @@ document.addEventListener("keydown", (e) => {
     try {
       console.log(seed, elapsed);
       console.log(seed);
-      const response = await fetch("/saveTime/", {
+      const response = await fetch("/api/saveTime/", {
           method: "POST",
           headers: { 
             "Content-Type": "application/json" ,

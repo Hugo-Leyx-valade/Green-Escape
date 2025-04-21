6128f6db07aadc4051b160ae4b8e8523fea20241
Hugo-Leyx-valade <hugo.leyx-valade@efrei.net>
2025-04-21 18:26:14 +0200

    lk

--- backend/GreenEscape-backend/api/static/scripts/scripts.js
+++ backend/GreenEscape-backend/api/static/scripts/scripts.js
@@ -198,7 +198,7 @@ document.addEventListener("keydown", (e) => {
     try {
       console.log(seed, elapsed);
       console.log(seed);
-      const response = await fetch("/saveTime/", {
+      const response = await fetch("/api/saveTime/", {
           method: "POST",
           headers: { 
             "Content-Type": "application/json" ,
